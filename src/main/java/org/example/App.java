f2b0fc029aa04c847f529b9f1b0af410e8a9b884
Alex Goodwin <goodwinalexjr@knights.ucf.edu>
2021-05-31 22:32:08 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner pizza = new Scanner(System.in);
+        System.out.print("How many people? ");
+        int a = pizza.nextInt();
+        System.out.print("How many pizzas do you have? ");
+        int b = pizza.nextInt();
+        System.out.print("How many slices per pizza? ");
+        int c = pizza.nextInt();
+        int d = b * c;
+        System.out.println(a + " people with " + b + " pizzas (" + d + " slices)");
+        int e = d / a;
+        System.out.println("Each person gets " + e + " pieces of pizza.");
+        int f = d % a;
+        System.out.println("There are " + f + " leftover pieces.");
+
+
+    }
+}
